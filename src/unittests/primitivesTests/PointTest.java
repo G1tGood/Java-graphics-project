b4bb98a10c84d2c3c9c5f52ccfc0c1604872a5cf
G1tGood <babayoff@g.jct.ac.il>
2023-04-13 16:35:12 +0300

    added tests for point and vector

---
+++ src/unittests/primitivesTests/PointTest.java
@@ -0,0 +1,91 @@
+package unittests.primitivesTests;
+
+import org.junit.jupiter.api.Test;
+import primitives.Point;
+import primitives.Vector;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+/**
+ * Unit tests for primitives.Point class
+ * @author Avishai Schachor and Yoav Babayof
+ */
+class PointTest {
+    /**
+     * Test method for {@link primitives.Point#subtract(primitives.Point)}.
+     */
+    @Test
+    void testSubtract() {
+        // ============ Equivalence Partitions Tests ==============
+        Point p1 = new Point(1,2,3);
+        Point p2 = new Point(6,4,8);
+
+        //TC01: test that subtraction result is right
+        assertEquals(new Vector(-5,-2,-5), p1.subtract(p2), "subtract() wrong result");
+
+        // =============== Boundary Values Tests ==================
+        // TC11: test zero vector from subtraction of the same point from itself
+        assertThrows(IllegalArgumentException.class, () -> p1.subtract(p1),
+                "subtract() for same point does not throw an exception");    }
+
+    /**
+     * Test method for {@link primitives.Point#add(primitives.Vector)}.
+     */
+    @Test
+    void testAdd() {
+        // ============ Equivalence Partitions Tests ==============
+        Point p1 = new Point(3,6,8);
+        Vector v1 = new Vector(6,4,8);
+
+        // TC01: test that addition result is right
+        assertEquals(new Point(9,10,16), p1.add(v1), "add() wrong result");
+
+        // =============== Boundary Values Tests ==================
+        Vector nP1 = new Vector(-3, -6, -8);
+
+        // TC11: test addition of point to its opposite (result zero)
+        try {
+            assertEquals(new Point(0,0,0), p1.add(nP1), "add() subtracting point from itself result in error");
+        } catch (IllegalArgumentException e) {
+            fail("add() subtracting point to itself result in ERROR");
+        }
+    }
+
+    /**
+     * Test method for {@link primitives.Point#distanceSquared(primitives.Point)}.
+     */
+    @Test
+    void testDistanceSquared() {
+        // ============ Equivalence Partitions Tests ==============
+        Point p1 = new Point(3,6,8);
+        Point p2 = new Point(15,2,9);
+
+        // TC01: test that distance squared result is right
+        assertEquals(161, p1.distanceSquared(p2), 0.00001, "distanceSquared() wrong result");
+
+        // =============== Boundary Values Tests ==================
+        // TC11: test distanceSquared zero from distance squared of the same point from itself
+        try {
+            assertTrue(primitives.Util.isZero(p1.distanceSquared(p1)), "distanceSquared() when equals 0 wrong result");
+        } catch (IllegalArgumentException e) {
+            fail("distanceSquared() distance from point to itself ERROR");
+        }
+    }
+
+    /**
+     * Test method for {@link primitives.Point#distance(primitives.Point)}.
+     */
+    @Test
+    void testDistance() {
+        // ============ Equivalence Partitions Tests ==============
+        Point p1 = new Point(3,6,8);
+        Point p2 = new Point(15,2,9);
+
+        //TC01: test that distance result is right
+        assertEquals(161, java.lang.Math.pow(p1.distance(p2), 2),0.00001, "distance() wrong result");
+
+        // =============== Boundary Values Tests ==================
+        // TC11: test distance zero from distance of the same point from itself
+        assertTrue(primitives.Util.isZero(p1.distance(p1)), "distance() when equals 0 wrong result");
+    }
+}
---
+++ src/unittests/primitivesTests/VectorTest.java
@@ -0,0 +1,114 @@
+package unittests.primitivesTests;
+
+import org.junit.jupiter.api.Test;
+import primitives.Vector;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+/**
+ * Unit tests for primitives.Vector class
+ * @author Avishai Schachor and Yoav Babayof
+ */
+class VectorTest {
+    /**
+     * Test method for {@link primitives.Vector#scale(double)}.
+     */
+    @Test
+    void testScale() {
+        // ============ Equivalence Partitions Tests ==============
+        Vector v1 = new Vector(3, 6, 7);
+
+        //TC01: test that scaling a vector by a positive factor is right
+        assertEquals(new Vector(3.6,7.2,8.4), v1.scale(1.2), "scale() wrong result for scaling by a positive factor");
+
+        //TC02: test that scaling a vector by a negative factor is right
+        assertEquals(new Vector(-1.5,-3,-3.5), v1.scale(-0.5), "scale() wrong result for scaling by a negative factor");
+
+        // =============== Boundary Values Tests ==================
+        //TC11: test scaling a vector by a factor of 0
+        assertThrows(IllegalArgumentException.class, () -> v1.scale(0),
+                "scale() for scaling by 0 does not throw an exception");
+    }
+
+    /**
+     * Test method for {@link primitives.Vector#dotProduct(primitives.Vector)}.
+     */
+    @Test
+    void testDotProduct() {
+        Vector v1 = new Vector(3, 6, 7);
+
+        // ============ Equivalence Partitions Tests ==============
+        //TC01: Test that dot product of two vector with a acute angle is proper
+        Vector v2 = new Vector(2, 7, 1);
+        assertEquals(2*3 + 6*7 + 7*1, v1.dotProduct(v2), 0.00001, "dotProduct() wrong result for acute angle");
+
+        //TC02: test that dot product of two vectors with a obtuse angle is proper
+        Vector v3 = new Vector(-5,2,4);
+        assertEquals((-5)*3+2*6+4*7,v1.dotProduct(v3),0.00001,"dotProduct() wrong result for obtuse angle" );
+
+
+        // =============== Boundary Values Tests ==================
+        //TC11: test dot product two orthogonal vectors
+        Vector v4 = new Vector(-6, -3, 0);
+        assertThrows(IllegalArgumentException.class, () -> v1.dotProduct(v4),
+                "dotProduct() for two orthogonal vectors does not throw an exception");
+
+        //TC12: test that dot product of two vectors when one of the vectors is a unit vector
+        assertEquals((0.8*3+0.6*6+0*7), v1.dotProduct(new Vector(4,3,0)),0.00001, "dotProduct() wrong result when one of the vectors is a unit vector");
+    }
+
+    /**
+     * Test method for {@link primitives.Vector#crossProduct(primitives.Vector)}.
+     */
+    @Test
+    void testCrossProduct() {
+        Vector v1 = new Vector(1, 2, 3);
+
+        // ============ Equivalence Partitions Tests ==============
+        Vector v2 = new Vector(0, 3, -2);
+        Vector vr = v1.crossProduct(v2);
+
+        // TC01: Test that length of cross-product is proper
+        assertEquals(v1.length() * v2.length(), vr.length(), 0.00001, "crossProduct() wrong result length");
+
+        // TC02: Test cross-product result orthogonality to its operands
+        assertTrue(primitives.Util.isZero(vr.dotProduct(v1)), "crossProduct() result is not orthogonal to 1st operand");
+        assertTrue(primitives.Util.isZero(vr.dotProduct(v2)), "crossProduct() result is not orthogonal to 2nd operand");
+
+        // =============== Boundary Values Tests ==================
+        // TC11: test zero vector from cross product of co-lined vectors
+        Vector v3 = new Vector(-2, -4, -6);
+        assertThrows(IllegalArgumentException.class, () -> v1.crossProduct(v3),
+                "crossProduct() for parallel vectors does not throw an exception");
+    }
+
+    /**
+     * Test method for {@link Vector#lengthSquared()}.
+     */
+    @Test
+    void testLengthSquared() {
+        // ============ Equivalence Partitions Tests ==============
+        // TC01: Test that length squared result of vector is proper
+        assertEquals(115, new Vector(3, 5, 9).lengthSquared(), 0.00001, "distanceSquared() wrong result");
+    }
+
+    /**
+     * Test method for {@link Vector#length()}.
+     */
+    @Test
+    void testLength() {
+        // ============ Equivalence Partitions Tests ==============
+        // TC01: Test that length result of vector is proper
+        assertEquals( 115,java.lang.Math.pow((new Vector (3, 5, 9)).length(), 2),0.00001,"length() wrong result");
+    }
+
+    /**
+     * Test method for {@link Vector#normalize()}.
+     */
+    @Test
+    void testNormalize() {
+        // ============ Equivalence Partitions Tests ==============
+        // TC01: Test that vector normalization is proper
+        assertEquals(new Vector(0.6, 0.8, 0), new Vector(3, 4, 0).normalize(), "normalize() wrong result");
+    }
+}
